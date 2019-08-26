5a10c9840ae241942934f106acf80dce83b43b90
Denali Balser <denali.balser167@gmail.com>
2019-08-26 14:29:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,15 @@ class Song
     @@artists.uniq
   end
 
-  def artist_count
-    @@artists.map do |
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else artist_count[artist] = 1
+      end
+    end  
+  end
+
 
 end
