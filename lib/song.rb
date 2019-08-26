21b1fe8db1c71dd1d22c1180ed21592e12b119bc
Denali Balser <denali.balser167@gmail.com>
2019-08-26 14:16:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,15 @@
 class Song
   attr_accessor :name, :artist, :genre
-  
+
   @@count = 0
   @@genres = []
   @@artists = []
 
   def initialize(name, artist, genre)
     @@count += 1
+    @name = name
+    @artist = artist
+    @genre = genre 
     @@genres << genre
     @@artists << artist
 
